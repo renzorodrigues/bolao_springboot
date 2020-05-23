782ece474118ee37154f676bec6473f8f02bd13b
Renzo <renzors@gmail.com>
2020-05-23 19:44:58 -0300

    first commit

---
+++ src/main/kotlin/com/renzo/bolao/BolaoApplication.kt
@@ -0,0 +1,11 @@
+package com.renzo.bolao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BolaoApplication
+
+fun main(args: Array<String>) {
+	runApplication<BolaoApplication>(*args)
+}
---
+++ src/test/kotlin/com/renzo/bolao/BolaoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.renzo.bolao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BolaoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
