fce0dd3a01e6154c77660c01c8d7430fd3725ded
Renzo <renzors@gmail.com>
2020-05-23 23:15:20 -0300

    adding User domain and H2 db

--- src/main/kotlin/com/renzo/bolao/BolaoApplication.kt
+++ src/main/kotlin/com/renzo/bolao/BolaoApplication.kt
@@ -1,5 +1,6 @@
 package com.renzo.bolao
 
+import com.renzo.bolao.domain.User
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +9,6 @@ class BolaoApplication
 
 fun main(args: Array<String>) {
 	runApplication<BolaoApplication>(*args)
+
+
 }
--- src/main/kotlin/com/renzo/bolao/controllers/UserController.kt
+++ src/main/kotlin/com/renzo/bolao/controllers/UserController.kt
@@ -1,10 +1,16 @@
 package com.renzo.bolao.controllers
 
+import com.renzo.bolao.domain.User
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class UserController{
+class UserController {
     @GetMapping("/hello")
-    fun show() = "hello"
+    fun show(): List<User> {
+        val user1 = User(12,"renzors@gmail.com","Renzo")
+        val user2 = User(23, "maria@uol.com", "Julianna")
+        val lista = listOf<User>(user1, user2)
+        return lista
+    }
 }
